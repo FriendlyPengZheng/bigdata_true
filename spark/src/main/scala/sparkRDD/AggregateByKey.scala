5a4f103b9ad4d7f91cdf18fe846ec20f67919a16
friendly <18627239055@163.com>
2020-01-03 14:11:58 +0800

    aggregateByKey Demo

---
+++ spark/src/main/scala/sparkRDD/AggregateByKey.scala
@@ -0,0 +1,34 @@
+package sparkRDD
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object AggregateByKey {
+    def main(args: Array[String]): Unit = {
+        val conf: SparkConf = new SparkConf().setAppName("aggre").setMaster("local[*]")
+        val sc:SparkContext = new SparkContext(conf)
+        val input: RDD[String] = sc.makeRDD(List("aa","bb","cc","bb","bb","bb","cc","dd","a"))
+        /*val agg: (Int, Int) = input.aggregate(
+            (0, 0))(
+            (x, y) => (x._1 + y, x._2 + 1),
+            (x, y) => (x._1 + y._1, x._2 + y._2))
+        val aggbk: RDD[(Int, (Int, Int))] = input.map((_,1)).aggregateByKey(
+            (0,0))(
+            (x, y) => (x._1 + y, x._2 + 1),
+            (x, y) => (x._1 + y._1, x._2 + y._2))*/
+        //println(agg)
+        val aggbk2: RDD[(String, Int)] = input.map((_,1)).aggregateByKey(
+            0)(
+            (x,y)=>(x+y),
+            (x,y)=>(x+y))
+        aggbk2.sortByKey().collect.foreach(println)
+        println("-----------------------")
+
+        /**
+          * foldByKey是aggregateByKey的特例
+          * seqOp和combOp一样的时候两个等价
+          */
+        input.map((_,1)).foldByKey(0)((x,y)=>(x+y))
+        //aggbk.sortByKey().collect.foreach(println(_))
+    }
+}
